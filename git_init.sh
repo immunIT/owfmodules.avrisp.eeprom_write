6e7fd91f31e889fd2eadb368a05f9aaf49006931
ghecko <ghecko78@gmail.com>
2020-06-30 15:52:30 +0200

    Adding git_init script

---
+++ git_init.sh
@@ -0,0 +1,3 @@
+git remote set-url origin --push --add git@github.com:immunIT/owfmodules.avrisp.eeprom_write.git
+git remote set-url origin --push --add git@bitbucket.org:octowire/owfmodules.avrisp.eeprom_write.git
+git remote -v
